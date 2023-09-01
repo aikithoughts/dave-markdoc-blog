2f81bfefd0f52396d941b88c55e7cc3cd7f1a54c
Dave Shevitz <shevitz@stripe.com>
2023-09-01 15:01:06 -0700

    fix a few layout issues

--- components/ReadingTime.jsx
+++ components/ReadingTime.jsx
@@ -2,7 +2,7 @@ const ReadingTime = ({ words }) => {
     const timeToRead = Math.round(words / 200);
     return (
         <div className="readingtime">
-            Estimated reading time: {timeToRead} {timeToRead <2 ? "minute" : "minute"}
+            Estimated reading time: {timeToRead} {timeToRead <2 ? "minute" : "minutes"}
         </div>
     )
 }
